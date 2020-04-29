d89a0dda201c5eb882a401fa9fdd8197de7d3a55
Prateek Parihar <prateek@Prateeks-MacBook-Pro.local>
2020-04-28 22:22:14 -0400

    -- removing cache data

--- public/component---src-pages-notes-js-6955c89a421282d505fb.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[7],{FWeN:function(e,t,n){"use strict";n.r(t);var a=n("q1tI"),r=n.n(a),c=n("Bl7J");t.default=function(){return r.a.createElement(c.a,null,r.a.createElement("h1",{style:{textAlign:"center",textTransform:"capitalize",marginTop:"4rem"}},"notes"))}}}]);
-//# sourceMappingURL=component---src-pages-notes-js-6955c89a421282d505fb.js.map
